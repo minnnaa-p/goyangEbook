d0db45f1dc302625d2a39743745c02a46b52a150
minnnaa-p <minaproject01@gmail.com>
2023-04-01 12:03:55 +0900

    Update memberJoin.js

--- js/memberJoin.js
+++ js/memberJoin.js
@@ -1,50 +1,53 @@
+$(function(){
+
+    let choice; //option 변경값
+    //이메일 도메인 select 값이 변경 될 떄 
+    $('#domainList').change (function(){
+        choice=$('#domainList>option:selected').text();
+        if(choice!='직접입력'){
+            $('#emailDomain').attr({
+                disabled: 'disabled',
+                value : choice
+            });
+        } else {
+            $('#emailDomain').removeAttr('disabled').attr('value','');
+        }
+    });
+    /*
+    let domain=$('#domainList');  //이메일 도메인 select id
+    $(domain).change(function(){
+        choice=$('#domainList>option:selected').text();
+        if(choice!='직접입력'){
+            $('#emailDomain').attr({
+                disabled: 'disabled',
+                value : choice
+            });
+        } else {
+            $('#emailDomain').removeAttr('disabled').attr('value','');
+        }
+    });
+    */
+    /*let requiredInput=$('#joinForm input:required');
+    let requirdSelect=('#joinForm select:required');
+
+    console.log(requiredInput);
+    console.log(requirdSelect);
+    */
+
+    //우편번호 찾기 popup
+    $('.zipcodePop').click(function(){
+        openPopup();
+    });
+    //우편번호 찾기 popup 끌때 
+    $('.popupClose').click(function (){
+        closePopup();
+    });
+    //우편번호 찾기 검색버튼 누를 떄
+    $('.popupContent a').click(function(){
+        searchZipcode();
+    });
 
 
-let choice; //option 변경값
-//이메일 도메인 select 값이 변경 될 떄 
-$('#domainList').change (function(){
-    choice=$('#domainList>option:selected').text();
-    if(choice!='직접입력'){
-        $('#emailDomain').attr({
-            disabled: 'disabled',
-            value : choice
-        });
-    } else {
-        $('#emailDomain').removeAttr('disabled').attr('value','');
-    }
-});
-/*
-let domain=$('#domainList');  //이메일 도메인 select id
-$(domain).change(function(){
-    choice=$('#domainList>option:selected').text();
-    if(choice!='직접입력'){
-        $('#emailDomain').attr({
-            disabled: 'disabled',
-            value : choice
-        });
-    } else {
-        $('#emailDomain').removeAttr('disabled').attr('value','');
-    }
-});
-*/
-/*let requiredInput=$('#joinForm input:required');
-let requirdSelect=('#joinForm select:required');
-
-console.log(requiredInput);
-console.log(requirdSelect);
-*/
-
-//우편번호 찾기 popup
-$('.zipcodePop').click(function(){
-    openPopup();
-});
-//우편번호 찾기 popup 끌때 
-$('.popupClose').click(function (){
-    closePopup();
-});
-//우편번호 찾기 검색버튼 누를 떄
-$('.popupContent a').click(function(){
-    searchZipcode();
 });
 
 //우편번호찾기 popup open함수
